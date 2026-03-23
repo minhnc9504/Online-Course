445efe14cacc99392c6bf8c2e3fbc66c57969af1
Cong Minh Nguyen <minhnc9504@gmail.com>
2026-03-23 14:34:40 +0700

    Add Instructor and Learner models with fields

--- myproject/models.py
+++ myproject/models.py
@@ -86,3 +86,26 @@ class Enrollment(models.Model):
     date_enrolled = models.DateField(default=timezone.now)
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
+class Instructor(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    full_time = models.BooleanField(default=True)
+    total_learners = models.IntegerField()
+
+    def __str__(self):
+        return self.user.username
+
+class Learner(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    STUDENT = 'student'
+    DEVELOPER = 'developer'
+    DATA_SCIENTIST = 'data_scientist'
+    OCCUPATION_CHOICES = [
+        (STUDENT, 'Student'),
+        (DEVELOPER, 'Developer'),
+        (DATA_SCIENTIST, 'Data Scientist')
+    ]
+    occupation = models.CharField(max_length=20, choices=OCCUPATION_CHOICES, default=STUDENT)
+    social_link = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.user.username + "," + self.occupation
